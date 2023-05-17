241e3c608c18314b48e1f03c4077ebdeb8889ae0
Hwang Su Jin <hang3137@naver.com>
2023-05-17 23:39:09 +0900

    feat : BasePlayer 기본 입력/움직임 구현

--- SpikeBeach/Source/SpikeBeach/BasePlayer.cpp
+++ SpikeBeach/Source/SpikeBeach/BasePlayer.cpp
@@ -2,26 +2,67 @@
 
 
 #include "BasePlayer.h"
+#include "CustomPlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
 ABasePlayer::ABasePlayer()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SetSuperSettings();
+
+	SetPlayerAttributes();
+
+	SetCapsuleComponent();
+
+	SetCharacterMovement();
+
+	SetCamera();
+
+	SetInputAction();
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 }
 
 // Called when the game starts or when spawned
 void ABasePlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//Add Input Mapping Context
+	if (ACustomPlayerController* PlayerController = Cast<ACustomPlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
 void ABasePlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (bIsGauging)
+	{
+		Gauge = Gauge < 100.0f ? Gauge + DeltaTime * GaugeSpeed : 100.0f;
+		UE_LOG(LogTemp, Log, TEXT("Gauge : %f"), Gauge);
+	}
+	else
+	{
+		Gauge = 0;
+	}
 
 }
 
@@ -30,5 +71,162 @@ void ABasePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+
+		// Clicking
+		EnhancedInputComponent->BindAction(ClickAction, ETriggerEvent::Triggered, this, &ABasePlayer::ClickTriggered);
+		EnhancedInputComponent->BindAction(ClickAction, ETriggerEvent::Completed, this, &ABasePlayer::ClickCompleted);
+
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABasePlayer::Move);
+
+		// Sprint
+		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Triggered, this, &ABasePlayer::SprintTriggered);
+		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Completed, this, &ABasePlayer::SprintCompleted);
+	}
+
 }
+
+void ABasePlayer::SetSuperSettings()
+{
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
+
+void ABasePlayer::SetPlayerAttributes()
+{
+	PlayerTurn = EPlayerTurn::PT_RECEIVE;
+
+	GaugeSpeed = 100.0f;
+
+	WalkSpeed = 500.0f;
+	SprintSpeed = 700.0f;
+}
+
+void ABasePlayer::SetCapsuleComponent()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 90.0f);
+}
+
+void ABasePlayer::SetCharacterMovement()
+{
+	
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+}
+
+void ABasePlayer::SetCamera()
+{
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character
+	CameraBoom->SocketOffset = FVector(0.0f, 0.0f, 150.0f);
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->SetRelativeRotation({ -10.f, 0.f, 0.f });
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+}
+
+void ABasePlayer::SetInputAction()
+{
+	//ConstructorHelpers::FObjectFinder<UInputMappingContext>DEFAULT_CONTEXT(TEXT("/Game/Content/Character/Input/IMC_Player.IMC_Player"));
+	//if (DEFAULT_CONTEXT.Succeeded())
+	//{
+	//	DefaultMappingContext = DEFAULT_CONTEXT.Object;
+	//}
+	//
+	//ConstructorHelpers::FObjectFinder<UInputAction>MOVE_ACTION(TEXT("/Game/Content/Character/Input/Action/IA_Move.IA_Move"));
+	//if (MOVE_ACTION.Succeeded())
+	//{
+	//	MoveAction = MOVE_ACTION.Object;
+	//}
+}
+
+void ABasePlayer::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ABasePlayer::ClickTriggered(const FInputActionValue& Value)
+{
+	if (!bIsGauging)
+	{
+		bIsGauging = true;
+		FName Direction = FName(TEXT("Left"));
+		switch (PlayerTurn)
+		{
+		case EPlayerTurn::PT_RECEIVE:
+			PlayAnimMontage(ReceiveMontage, 1.0f, Direction);
+			PlayerTurn = EPlayerTurn::PT_TOSS;
+			break;
+		case EPlayerTurn::PT_TOSS:
+			PlayAnimMontage(TossMontage, 1.0f, Direction);
+			PlayerTurn = EPlayerTurn::PT_SPIKE;
+			break;
+		case EPlayerTurn::PT_SPIKE:
+			PlayAnimMontage(SpikeMontage, 1.0f, FName(TEXT("FullSpike")));
+			PlayerTurn = EPlayerTurn::PT_RECEIVE;
+			break;
+		}
+	}
+
+}
+
+void ABasePlayer::ClickCompleted(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Log, TEXT("Click Complete"));
+
+	bIsGauging = false;
+}
+
+void ABasePlayer::SprintTriggered(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Log, TEXT("Sprint Triggered"));
+
+	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+}
+
+void ABasePlayer::SprintCompleted(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Log, TEXT("Sprint Completed"));
+
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+}
+
 
