536db62e2abb9641c3da0284320bf1f70edef1e5
Hwang Su Jin <hang3137@naver.com>
2023-05-18 12:21:40 +0900

    feat : SphereCollision을 가지고 있는 Ball 액터 클래스 생성

---
+++ SpikeBeach/Source/SpikeBeach/Ball.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ball.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ABall::ABall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereCollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
+	SetRootComponent(SphereCollisionComponent);
+	SphereCollisionComponent->SetSphereRadius(16.0f);
+	SphereCollisionComponent->SetSimulatePhysics(true);
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComponent->SetupAttachment(SphereCollisionComponent);
+}
+
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ SpikeBeach/Source/SpikeBeach/Ball.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Ball.generated.h"
+
+UCLASS()
+class SPIKEBEACH_API ABall : public AActor
+{
+	GENERATED_BODY()
+private:
+	/** Sphere Collision Component */
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class USphereComponent* SphereCollisionComponent;
+
+	/** Mesh Component */
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* MeshComponent;
+public:	
+	// Sets default values for this actor's properties
+	ABall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
