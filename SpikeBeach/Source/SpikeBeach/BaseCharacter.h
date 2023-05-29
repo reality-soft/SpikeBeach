13f102538d0a2bf75398a6dcaed56da93ab14cfe
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-05-29 21:09:58 +0900

    feat : 마우스로 공 발사 위치 조정

--- SpikeBeach/Source/SpikeBeach/BaseCharacter.h
+++ SpikeBeach/Source/SpikeBeach/BaseCharacter.h
@@ -150,6 +150,7 @@ public:
 public:
 	UPROPERTY(BlueprintReadWrite, Category = "Ball Attach Component")
 		class USceneComponent* ball_attachment_;
+
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = Animation)
 		class UDataTable* AnimOffsetData;
