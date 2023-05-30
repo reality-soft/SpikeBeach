895b2359aa216ee458f86a6fdf5532e72d5eae50
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-05-30 18:41:19 +0900

    feat : 머지 후 오류 수정

--- SpikeBeach/Source/SpikeBeach/World/VolleyBallTeam.h
+++ SpikeBeach/Source/SpikeBeach/World/VolleyBallTeam.h
@@ -58,48 +58,6 @@ public:
 		void SetTeamCourt(ECourtName court_name) { court = court_name; }
 #pragma endregion
 
-	UFUNCTION(BlueprintCallable)
-		bool AddPlayerToTeam(ABaseCharacter* player) {
-		if (left_side_player == nullptr)
-		{
-			left_side_player = player;
-			left_side_player->SetMyTeam(this);
-			return true;
-		}
-
-		if (right_side_player == nullptr)
-		{
-			right_side_player = player;
-			right_side_player->SetMyTeam(this);
-			return true;
-		}
-
-		return false;
-	}
-
-	UFUNCTION(BlueprintCallable)
-		void WinSinglePoint() {
-		set++;
-	}
-
-	UFUNCTION(BlueprintCallable)
-		void WinSetPoint() {
-		score++;
-	}
-
-	UFUNCTION(BlueprintCallable)
-		void SwapPlayerPos() {
-		ABaseCharacter* temp = right_side_player;
-		right_side_player = left_side_player;
-		left_side_player = temp;
-	}
-
-	UFUNCTION(BlueprintCallable, Category = "Game Play")
-		void UpdateBallCursor(FVector2D cursor);
-
-	UFUNCTION(BlueprintCallable, Category = "Game Play")
-		void ClearBallCursor();
-
 #pragma region GETTER
 public:
 	UFUNCTION(BlueprintCallable)
@@ -127,10 +85,10 @@ public:
 		bool AddPlayerToTeam(ABaseCharacter* player);
 
 	UFUNCTION(BlueprintCallable)
-		void WinSinglePoint();
+		void WinScore();
 
 	UFUNCTION(BlueprintCallable)
-		void WinSetPoint();
+		void WinSet();
 
 	UFUNCTION(BlueprintCallable)
 		void SwapPlayerPos();
