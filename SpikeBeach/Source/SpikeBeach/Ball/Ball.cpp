09b78cd6fc61c2fb5e1a64bcd968e6ae88284455
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-06-02 04:37:27 +0900

    chore: 공 움직임 수정

--- SpikeBeach/Source/SpikeBeach/Ball/Ball.cpp
+++ SpikeBeach/Source/SpikeBeach/Ball/Ball.cpp
@@ -122,7 +122,7 @@ FVector ABall::SpikeMovement(float power, const FVector& start_pos, const FVecto
 
 FVector ABall::ReceiveMovement(float power, const FVector& start_pos, const FVector& end_pos, EBallState ball_state)
 {
-	power = (0.6 - 0.3) * (1.0 - power) + 0.3;
+	power = (0.5 - 0.3) * (1.0 - power) + 0.3;
 	FVector velocity;
 	
 	UGameplayStatics::SuggestProjectileVelocity_CustomArc(SphereCollisionComponent, velocity, start_pos, end_pos, 0.0f, power);
@@ -146,7 +146,7 @@ FVector ABall::ReceiveMovement(float power, const FVector& start_pos, const FVec
 
 FVector ABall::TossMovement(float power, const FVector& start_pos, const FVector& end_pos, EBallState ball_state)
 {
-	power = (0.5 - 0.3) * (1.0 - power) + 0.3;
+	power = (0.4 - 0.3) * (1.0 - power) + 0.3;
 	FVector velocity;
 
 	UGameplayStatics::SuggestProjectileVelocity_CustomArc(SphereCollisionComponent, velocity, start_pos, end_pos, 0.0f, power);
@@ -194,7 +194,7 @@ FVector ABall::FloatingMovement(float power, const FVector& start_pos, const FVe
 
 FVector ABall::DigMovement(float power, const FVector& start_pos, const FVector& end_pos, EBallState ball_state)
 {
-	power = (0.7 - 0.5) * (1.0 - power) + 0.5;
+	power = (0.5 - 0.4) * (1.0 - power) + 0.4;
 	FVector velocity;
 
 	UGameplayStatics::SuggestProjectileVelocity_CustomArc(SphereCollisionComponent, velocity, start_pos, end_pos, 0.0f, power);
