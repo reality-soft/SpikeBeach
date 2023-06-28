4dc2658be23bd81009a45221d784e91fc83f2a9d
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-06-29 01:05:26 +0900

    chore: Http 통신에 필요한 언리얼 모듈 추가

--- SpikeBeach/Source/SpikeBeach/SpikeBeach.Build.cs
+++ SpikeBeach/Source/SpikeBeach/SpikeBeach.Build.cs
@@ -8,7 +8,7 @@ public class SpikeBeach : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Niagara", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Niagara", "HeadMountedDisplay", "EnhancedInput", "HTTP", "Json" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
