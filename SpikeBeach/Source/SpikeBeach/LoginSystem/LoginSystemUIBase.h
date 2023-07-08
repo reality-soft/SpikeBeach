3ae4eba434278b6fb8d98355e278b7146301094d
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-07-09 01:25:32 +0900

    feat: 회원가입 관련 에러처리를 위한 enum추가

--- SpikeBeach/Source/SpikeBeach/LoginSystem/LoginSystemUIBase.h
+++ SpikeBeach/Source/SpikeBeach/LoginSystem/LoginSystemUIBase.h
@@ -7,6 +7,38 @@
 #include "Http.h"
 #include "LoginSystemUIBase.generated.h"
 
+UENUM(BlueprintType)
+enum class EErrorCode : uint8
+{
+    // Generic Error : 0 ~ 49
+    None = 0,
+    AccountDbError = 1,
+    SessionError = 2,
+    RoomDbError = 3,
+    GameDbError = 4,
+    ServerError = 5,
+    InvalidBodyForm = 6,
+    InvalidPacketForm = 7,
+
+    // Auth Error : 50 ~ 99
+    InvalidId = 50,
+    InvalidIdFormat = 51,
+    DuplicatedId = 52,
+    InvalidToken = 53,
+    AreadyLogin = 54,
+    WorngPassword = 55,
+    WorngClientVersion = 56,
+
+    // room Error : 100 ~
+    RoomLeaveSuccess = 100,
+    RoomNotExist = 101,
+    RoomFull = 102,
+    UserAlreadyInRoom = 103,
+    UserNotInRoom = 104,
+    UserAlreadyReady = 105,
+    UserNotReady = 106
+};
+
 /**
  * 
  */
@@ -14,4 +46,5 @@ UCLASS()
 class SPIKEBEACH_API ULoginSystemUIBase : public UUserWidget
 {
 	GENERATED_BODY()
+
 };
