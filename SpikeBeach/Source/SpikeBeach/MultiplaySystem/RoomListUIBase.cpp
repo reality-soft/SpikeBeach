75f8dc64692f2e78e35c68599e0fb5e995b1188a
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-07-13 01:34:31 +0900

    feat: 방 리스트 출력 UI 클래스 구현

---
+++ SpikeBeach/Source/SpikeBeach/MultiplaySystem/RoomListUIBase.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RoomListUIBase.h"
+
+void URoomListUIBase::RoomListRequest(FString userAssignedId, FString token, FString clientVersion)
+{
+	FHttpRequestRef Request = FHttpModule::Get().CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &URoomListUIBase::OnRoomListResponseRecevied);
+
+	TSharedRef<FJsonObject> RequestObj = MakeShared<FJsonObject>();
+	RequestObj->SetStringField("userAssignedId", userAssignedId);
+	RequestObj->SetStringField("token", token);
+	RequestObj->SetStringField("clientVersion", clientVersion);
+
+	FString RequestBody;
+	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&RequestBody);
+	FJsonSerializer::Serialize(RequestObj, Writer);
+
+	Request->SetURL("http://52.197.242.93/Room/Listup");
+	Request->SetVerb("POST");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetContentAsString(RequestBody);
+
+	UE_LOG(LogTemp, Display, TEXT("Request : %s"), *RequestBody);
+
+	Request->ProcessRequest();
+}
+
+void URoomListUIBase::OnRoomListResponseRecevied(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSuccessfully)
+{
+}
