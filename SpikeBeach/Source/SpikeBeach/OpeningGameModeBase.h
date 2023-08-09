9b2ab36ef629c4ae1719667aa0258d83eb938ce4
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-08-10 00:30:38 +0900

    feat: 게임 모드를 C++ 코드에서 접근하기 위한 게임모드 부모 클래스 구현

--- SpikeBeach/Source/SpikeBeach/OpeningGameModeBase.h
+++ SpikeBeach/Source/SpikeBeach/OpeningGameModeBase.h
@@ -13,5 +13,17 @@ UCLASS()
 class SPIKEBEACH_API AOpeningGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+		void EnterRoom(const FString& roomInfoString);
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void ExitRoom();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void AddUserToRoom(const FString& enterUserNick);
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void RemoveUserFromRoom(const FString& enterUserNick, const FString& newHostNick);
 };
