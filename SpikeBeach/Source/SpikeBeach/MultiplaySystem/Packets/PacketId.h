9771b0d118b1a2cbc114947bbe3d7b447cf6d05f
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-08-28 15:39:47 +0900

    feat: TCP 연결을 위한 패킷 id 열거형 작성

---
+++ SpikeBeach/Source/SpikeBeach/MultiplaySystem/Packets/PacketId.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+enum PacketId
+{
+	// Generic 0 ~ 99
+	CLOSE_CONN = 1,
+	ERROR_OCCUR = 11,
+	DISCONN_REQ = 12,
+	DISCONN_RES = 13,
+	DISCONN_NTF = 14,
+	GAME_TIMEOUT_NTF = 15,
+
+	GAME_ENTER_REQ = 101,
+	GAME_ENTER_NTF = 102,
+	GAME_ENTER_RES = 103,
+	GAME_LEAVE_REQ = 104,
+	GAME_LEAVE_RES = 105,
+	GAME_LEAVE_NTF = 106,
+	GAME_START_NTF = 107,
+};
