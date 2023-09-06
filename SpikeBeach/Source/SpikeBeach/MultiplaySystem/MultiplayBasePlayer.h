da76e43a3251550be1729f1e309ba6ce2fd80635
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-09-06 17:11:55 +0900

    feat: 멀티 플레이를 위한 캐릭터 클래스 추가

---
+++ SpikeBeach/Source/SpikeBeach/MultiplaySystem/MultiplayBasePlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "MultiplayBasePlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPIKEBEACH_API AMultiplayBasePlayer : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
